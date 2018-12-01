fb4e3551d55a42aa824340f4290468a18d35fa23
Jose Angel De La Cruz <30847700+angellox@users.noreply.github.com>
2018-11-30 18:27:31 -0600

    Add files via upload

---
+++ Control Inventario/nbproject/Producto.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelo;
+
+/**
+ *
+ * @author Ángel Cruz
+ */
+public class Producto {
+    
+    int clave, cantidad;
+    Categoria categoria;
+    String nombre;
+    Marca marca;
+    boolean requiereRefrigeracion;
+    
+}

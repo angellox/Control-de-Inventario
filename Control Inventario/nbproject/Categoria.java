25d5af7dea32e37f04187cf048ae0667371caf1c
Angel Oropeza <31115892+AngelOropeza@users.noreply.github.com>
2018-12-02 22:27:20 -0600

    Add files via upload

--- Control Inventario/nbproject/Categoria.java
+++ Control Inventario/nbproject/Categoria.java
@@ -5,10 +5,42 @@
  */
 package modelo;
 
+
 /**
  *
- * @author Ángel Cruz
+ * @author qooki
  */
-public class Categoria {
+class Categoria {
+    //Atributos
+    private String nombre;
     
+    // Constructor 
+    protected Categoria(String _nombre)
+    {
+        this.nombre=_nombre;
+    }
+    
+    // M È T O D O S
+    public Categoria obtenerInstancia()
+    {
+        return this;
+    }
+    
+    @Override
+    public boolean equals(Object o)
+    {
+        if(o == this)
+                {
+                    return true;
+                }
+        return false;
+    }
+    
+    @Override
+    public String toString()
+    {
+        return "Nombre: "+nombre;
+    }
+            
 }
+
